4fff76ea72e5864517ea18d63f98c6f33f42c32b
levintoo <tooklevin@gmail.com>
2022-03-29 17:18:18 +0300

    livewire actions

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -6,8 +6,19 @@ use Livewire\Component;
 
 class HomeComponent extends Component
 {
+    public $pounds;
+    public function mount()
+    {
+        $this->pounds = 100;
+    }
+
     public function render()
     {
-        return view('livewire.home-component')->layout('layouts.base');
+        return view('livewire.home-component', ['pounds' => $this->pounds])->layout('layouts.base');
+    }
+    public function doSomething()
+    {
+        session()->flash('message', "value reduced to 5");
+        $this->pounds = 5;
     }
 }
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,6 +11,29 @@
     <link href="{{ asset('assets/css/style-sunset.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/images/logo.png') }}" rel="icon" />
     <title>#TeamUsername</title>
+    @livewireStyles
 </head>
+
+<body id="bodysunset">
+    {{--<div id="notificationWrap2" class="bg-sunset-red text-center text-white font-weight-bold py-2 overflow-hidden text-center">--}}
+    {{-- <p class="m-0 px-1 text-uppercase font-sm">JOIN IN WITH #TEAMUSERNAME AND TRACK YOUR PROGRESS&emsp;<a class="text-white btn btn-white-outline" href="https://actionnetwork.org/forms/teamseas-signup" target="_blank">SIGN UP</a></p>--}}
+    {{--</div>--}}
+    <header id="header">
+        <div id="navbar-header" class="navbar">
+            <div id="menuBar" class="w-100">
+                <div class="navbar-logo"></div>
+                <div class="navbar-menu-button"><button type="button" class="navbar-button" data-toggle="modal" data-target="#menuMain"><img src="{{ asset('assets/images/logo.png') }}" /></button></div>
+            </div>
+        </div>
+    </header>
     {{$slot}}
+    <!-- Javascript -->
+    @stack('modals')
+
+    @livewireScripts
+    <script src="{{ asset('assets/js/autocomplete.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/sweetalert2.js') }}?v=1.0.0"></script>
+    @stack('scripts')
+</body>
+
 </html>
