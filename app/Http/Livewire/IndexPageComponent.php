f7a657969ac2c9aa707dcdf295e43cc53e8b5121
levintoo <tooklevin@gmail.com>
2022-04-11 22:44:27 +0300

    implementing search in homepage

--- app/Http/Livewire/IndexPageComponent.php
+++ app/Http/Livewire/IndexPageComponent.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 
 class IndexPageComponent extends Component
 {
+
+    public $search;
+    public $name;
+    public function mount()
+    {
+
+    }
+    public function search()
+    {
+            session::flash('message','results found');
+    }
     public function render()
     {
-        return view('livewire.index-page-component')->layout('layouts.base');
+        if(is_null($this->name))
+        {
+            $users = "";
+        }else{
+            $users = User::where ( 'username', 'LIKE', '%' . $this->name . '%' )->orWhere ( 'name', 'LIKE', '%' . $this->name . '%' )->get();
+        }
+        return view('livewire.index-page-component',['users'=>$users])->layout('layouts.base');
     }
 }
--- resources/views/livewire/index-page-component.blade.php
+++ resources/views/livewire/index-page-component.blade.php
@@ -12,11 +12,8 @@
                                         <h4 class="card-title text-center text-uppercase font-lg">Get Me A Coffee</h4>
                                     </div>
                                     <div class="card-body text-center py-2">
-                                    <div class="alert alert-success" role="alert">{{ Auth::user()->username }}
-                                        </div>
                                     @if(Session::has('message'))
-                                            <div class="alert alert-success" role="alert">{{ Auth::user()->username }}jh
-                                                {{Session::get('message')}}</div>
+                                            <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
                                         @endif
                                         @if(Session::has('danger-message'))
                                             <div class="alert alert-danger" role="alert">{{Session::get('danger-message')}}</div>
@@ -25,13 +22,13 @@
                                         <div class="row">
 
                                             <div class="col-8 mb-1 px-1">
-                                                <input id="donateAmountOther" name="other_amount" value="" type="text" class="form-control custom-control h-100" maxlength="10" placeholder="john doe">
+                                                <input id="" name="search-value" value="" type="text" class="form-control custom-control h-100" maxlength="100" placeholder="john doe" wire:model="name">
                                             </div>
 
                                             <div class="col mb-1 px-1">
                                                 <div class="custom-control custom-checkbox h-100">
-                                                    <input name="pounds" value="100" type="button" class="custom-control-input" id="checkPounds100">
-                                                    <label id="donateAmount100" class="custom-control-label text-uppercase h-100 d-flex align-items-center justify-content-center" for="checkPounds100">search</label>
+                                                    <input name="pounds" value="100" type="button" class="custom-control-input" id="checkPounds100" wire:click="search()">
+                                                    <label id="donateAmount100" class="custom-control-label text-uppercase h-100 d-flex align-items-center justify-content-center" for="checkPounds100" wire:click="search()">search</label>
                                                 </div>
                                             </div>
                                         </div>
@@ -46,31 +43,28 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody class="datatable-body">
-                                                    <tr scope="row" data-mdb-index="0">
-                                                        <td style="" class="" data-mdb-field="field_0" false="">
-                                                            <img width="100px" height="100px" class="img-fluid rounded" src="{{ asset('assets/dashboard/images/banner/1.jpg') }}" alt="">
-                                                        </td>
-                                                        
-                                                        <td style="" class="" data-mdb-field="field_1" false=""><a href="/systemarchitect">System Architect</a></td>
+                                                    @if($users == "")
+                                                        <tr scope="row" data-mdb-index="0">
+                                                            <td style="" class="" data-mdb-field="field_0" false="">
+                                                                <img width="100px" height="100px" class="img-fluid rounded" src="{{ asset('assets/dashboard/images/banner/1.jpg') }}" alt="">
+                                                            </td>
 
-                                                        <td style="" class="" data-mdb-field="field_2" false=""> Edinburgh</td>
+                                                            <td style="" class="" data-mdb-field="field_1" false=""><a href="/systemarchitect">Nothing to show</a></td>
 
-                                                    </tr>
-                                                    <tr scope="row" data-mdb-index="1">
-                                                        <td style="" class="" data-mdb-field="field_0" false="">
-                                                            <img src="{{ asset('assets/dashboard/images/logo/favicon-icon.png') }}" alt="">
-                                                        </td>
-                                                        <td style="" class="" data-mdb-field="field_1" false="">Software Engineer</td>
-                                                        <td style="" class="" data-mdb-field="field_2" false="">Edinburgh</td>
+                                                            <td style="" class="" data-mdb-field="field_2" false="">Nothing to show</td>
 
-                                                    </tr>
-                                                    <tr scope="row" data-mdb-index="2">
-                                                        <td style="" class="" data-mdb-field="field_0" false="">
-                                                            <img src="{{ asset('assets/dashboard/images/logo/favicon-icon.png') }}" alt="">
-                                                        </td>
-                                                        <td style="" class="" data-mdb-field="field_1" false="">Office Manager</td>
-                                                        <td style="" class="" data-mdb-field="field_2" false="">London</td>
-                                                    </tr>
+                                                        </tr>
+                                                        @else
+                                                    @foreach($users as $user)
+                                                        <tr scope="row" data-mdb-index="1">
+                                                            <td style="" class="" data-mdb-field="field_0" false="">
+                                                                <img src="{{ asset('assets/dashboard/images/logo/favicon-icon.png') }}" alt="">
+                                                            </td>
+                                                            <td style="" class="" data-mdb-field="field_1" false=""><a href="/{{$user->username}}">{{$user->username}}</a></td>
+                                                            <td style="" class="" data-mdb-field="field_2" false="">{{$user->category}}</td>
+                                                        </tr>
+                                                    @endforeach
+                                                    @endif
 
                                                 </tbody>
                                             </table>
@@ -114,7 +108,6 @@
 </main>
 @push('scripts')
 <script>
-
 
 </script>
 @endpush
