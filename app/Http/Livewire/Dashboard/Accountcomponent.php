87253934269d8cde00e57fcfc142b05618d65b8e
levintoo <tooklevin@gmail.com>
2022-04-16 21:22:28 +0300

    few more components

---
+++ app/Http/Livewire/Dashboard/Accountcomponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard;
+
+use Livewire\Component;
+
+class Accountcomponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.accountcomponent');
+    }
+}
