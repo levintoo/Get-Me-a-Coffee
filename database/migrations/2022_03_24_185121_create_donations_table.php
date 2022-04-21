f3a2a6c03847c22858996c04c0b7a512883623bc
levintoo <tooklevin@gmail.com>
2022-04-21 22:37:22 +0300

    donation table mail validation remove and add userid val

--- database/migrations/2022_03_24_185121_create_donations_table.php
+++ database/migrations/2022_03_24_185121_create_donations_table.php
@@ -16,9 +16,9 @@ class CreateDonationsTable extends Migration
         Schema::create('donations', function (Blueprint $table) {
             $table->id();
             $table->string('userid',100);
-            $table->string('donorid',100);
+            $table->string('donorid',100)->unique();
             $table->string('name',100);
-            $table->string('email',100)->unique();
+            $table->string('email',100);
             $table->string('phone_number',100);
             $table->string('message',250);
             $table->string('anonymous',100)->comment("0 for none 1 for anonymous");
@@ -26,7 +26,7 @@ class CreateDonationsTable extends Migration
             $table->string('transaction_id');
             $table->bigInteger('amount')->unsigned();
             $table->string('status')->default('0')->comment('0 for initiated, 1 for donated');
-            $table->date('donated_at')->format('Y/m/d H:i:s');
+            $table->dateTime('donated_at')->format('Y/m/d H:i:s');
             $table->timestamps();
             $table->softDeletes();
         });
