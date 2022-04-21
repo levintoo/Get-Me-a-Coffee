39192f903174d0c179cbf090ae66833698b8375a
levintoo <tooklevin@gmail.com>
2022-04-21 22:36:43 +0300

    faking donation and saving transaction

--- app/Http/Livewire/PaymentDetails.php
+++ app/Http/Livewire/PaymentDetails.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Livewire;
 
+use App\Helpers\Helper;
+use App\Models\Donations;
+use App\Models\DonationTransactions;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 
@@ -33,10 +38,6 @@ class PaymentDetails extends Component
             session::flash('danger-message','An unexcpected error occured');
             return redirect('/');
         };
-        if (Session()->get('name_hidden') === null || Session()->get('name_hidden') === "") {
-            session::flash('danger-message','An unexcpected error occured');
-            return redirect('/');
-        };
     }
     public function render()
     {
@@ -45,7 +46,6 @@ class PaymentDetails extends Component
     public function updated($fields)
     {
         $this->validateOnly($fields, [
-            'name_hidden' => ['string', 'max:255'],
             'inputName' => ['required', 'string', 'max:255'],
             'inputEmail' => ['required', 'string', 'email', 'max:255'],
             'inputPhone' => ['required','regex:/^([0-9\s\-\+\(\)]*)$/','min:10', 'max:255'],
@@ -58,7 +58,6 @@ class PaymentDetails extends Component
     public function store(Request $request)
     {
         $request->validate([
-            'name_hidden' => 'string',
             'inputName' => ['required', 'string', 'max:255'],
             'inputEmail' => ['required', 'string', 'email', 'max:255'],
             'inputPhone' => ['required','regex:/^([0-9\s\-\+\(\)]*)$/','min:10', 'max:255'],
@@ -67,10 +66,56 @@ class PaymentDetails extends Component
             'username' => ['required', 'string', 'max:255'],
             'nameOnCard' => ['required', 'string', 'max:255']
         ]);
+        $donation = new Donations;
+        $user = User::select('userid')->where('username',$request->username)->first();
+        $donation->userid = $user->userid;
+        $donation->donorid = Helper::IDGenerator(new Donations(),'donorid',4,'DON');
+        $donation->name = $request->inputName;
+        $donation->email = $request->inputEmail;
+        $donation->phone_number = $request->inputPhone;
+        $donation->message = $request->inputMessage;
+        if($request->name_hidden != '')
+        {
+            $donation->anonymous = '1';
+        }
+        else{
+            $donation->anonymous = '0';
+        }
+        $donation->mode_of_payment = 'mpesa';
+        $donation->transaction_id = 'AFSDY12KSE';
+        $donation->amount = $request->amount;
+        $donation->status = '0';
+        $donation->donated_at = Carbon::now();
+        $donation->save();
+        $this->fakeTransaction($donation);
+        $this->clearSessions();
         return $request;
         // return redirect()->route('donation.credits');
     }
-    public function goBack(Request $request)
+    public function clearSessions()
+    {
+        session()->forget('amount');
+        session()->forget('username');
+        session()->forget('inputName');
+        session()->forget('inputEmail');
+        session()->forget('inputPhone');
+        session()->forget('inputMessage');
+        session()->forget('name_hidden');
+    }
+    public function fakeTransaction($donation)
+    {
+        $transaction = new DonationTransactions();
+        $transaction->userid = $donation->userid;
+        $transaction->donorid = $donation->donorid;
+        $transaction->amount = $donation->amount;
+        $transaction->type = 'credit';
+        $transaction->transaction_id = 'ASDAGH120';
+        $transaction->purpose = 'donation';
+        $transaction->status = '0';
+        $transaction->save();
+    }
+
+    public function goBack()
     {
         return redirect()->route('donation-payment');
     }
