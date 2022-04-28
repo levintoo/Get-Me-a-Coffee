8d20348a5673b53898af11b19d87e8b4073bfac8
levintoo <tooklevin@gmail.com>
2022-04-28 20:30:33 +0300

    displaying donations

--- app/Http/Livewire/Dashboard/Donationscomponent.php
+++ app/Http/Livewire/Dashboard/Donationscomponent.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire\Dashboard;
 
+use App\Models\Donations;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Donationscomponent extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard.donationscomponent');
+        $donations = Donations::where('userid','=',Auth::user()->userid)->where('status','=',0)->get();
+        return view('livewire.dashboard.donationscomponent',['donations'=>$donations]);
     }
 }
--- resources/views/livewire/dashboard/donationscomponent.blade.php
+++ resources/views/livewire/dashboard/donationscomponent.blade.php
@@ -18,7 +18,93 @@
     <!-- Container-fluid starts-->
     <div class="container-fluid default-dash">
         <div class="row">
-            donations goes here
+           <div class="col-xl-4 col-md-6 dash-35 dash-xl-100">
+                <div class="card ongoing-project">
+                    <div class="card-header card-no-border">
+                        <div class="media media-dashboard">
+                            <div class="media-body">
+                                <h5 class="mb-0">Messages From People        </h5>
+                            </div>
+                            <div class="icon-box onhover-dropdown"><i data-feather="more-horizontal"></i>
+                                <div class="icon-box-show onhover-show-div">
+                                    <ul>
+                                        <li> <a>
+                                                Done</a></li>
+                                        <li> <a>
+                                                Pending</a></li>
+                                        <li> <a>
+                                                Rejected</a></li>
+                                        <li> <a>In Progress</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body pt-0">
+                        <div class="table-responsive custom-scrollbar">
+                            <table class="table table-bordernone">
+                                <thead>
+                                <tr>
+                                    <th> <span>Name </span></th>
+                                    <th> <span>Date</span></th>
+                                    <th> <span>Message </span></th>
+                                    <th> <span>Status   </span></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($donations as $donation)
+                                    <tr>
+                                        <td>
+                                            <div class="media">
+                                                @php
+                                                    $random = rand(1,4);
+                                                    if($random == '1' ){
+                                                        $randomImg = "boy.png";
+                                                    }elseif($random == '2' ){
+                                                        $randomImg = "girl.png";
+                                                    }elseif($random == '3' ){
+                                                        $randomImg = "man.png";
+                                                    }elseif($random == '4' ){
+                                                        $randomImg = "woman.png";
+                                                    }
+                                                @endphp
+                                                <div class="square-box me-2"><img class="img-fluid b-r-5" src="{{ asset('assets/dashboard/images/avtar/'.$randomImg) }}" alt=""></div>
+                                                <div class="media-body ps-2">
+                                                    <div class="avatar-details"><a href="#">
+                                                            {{$donation->randomImg}}
+                                                            <h6>@if($donation->anonymous == '1')
+                                                                    {{$donation->name}}
+                                                                @else
+                                                                    {{"anonymous"}}
+                                                                @endif</h6></a><span>${{ number_format($donation->amount, 0)}}</span></div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="img-content-box">
+
+                                            <h6> {{ Carbon\Carbon::parse($donation->donated_at)->format('F j, Y') }}</h6><span>6 Days Ago</span>
+                                        </td>
+                                        <td>
+                                            <h6>{{$donation->message}}</h6>
+                                        </td>
+                                        <td>
+                                            @if($donation->status == '0')
+                                                <div class="badge badge-light-info my-1">new</div>
+                                            @endif
+{{--                                            <div class="badge badge-light-primary">Regular</div>--}}
+{{--                                                <div class="badge badge-light-info">In Progress</div>--}}
+{{--                                                <div class="badge badge-light-danger">Rejected</div>--}}
+{{--                                                <div class="badge badge-light-secondary">Pending</div>--}}
+
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
